32f348988e3dcdb890699c4d8bcc8661100060db
Farzeel Ahmad <farzeel.ahmed@logicielservice.com>
2026-02-11 14:55:46 +0500

    revert old changes

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 fetch("/config.json")
   .then((data) => data?.json?.())
@@ -8,3 +10,8 @@ fetch("/config.json")
       <App appConfig={appConfig} />
     );
   });
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
